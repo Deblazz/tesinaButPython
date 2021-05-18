7335b725121cd719040d9ca3d8d5cf12a70ba6c5
deblazz <gioco654@gmail.com>
2021-05-18 17:58:30 +0200

    Added image conversion from cifar10 dataset

--- aiTrainer.py
+++ aiTrainer.py
@@ -1,16 +1,58 @@
 import os
+
+import cv2
+import mxnet
+import numpy
 from tkinter import messagebox
 from tkinter import filedialog
 import shutil
 import os
-import downloader
+import pickle
 def train():
     answer = messagebox.askokcancel("Warning", "Training the AI is a time-consuming process, are you ready?")
 
     if answer == True:
-        saveNegatives()
-        savePositives()
-        #downloader.download()
+        answer = messagebox.askyesno("", "Do you want to access a CIFAR-10 compatible dataset?")
+        if answer == True:
+            openCifar10()
+        else:
+            saveNegatives()
+            savePositives()
+
+def getCifar10Cat():
+    categoriesPath = filedialog.askopenfilename(title="Select your batches.meta file", initialdir="/media/deblazz/CEBAE294BAE2787")
+    with open(categoriesPath, 'rb') as file:
+        cat = pickle.load(file)
+    return cat
+
+def saveCifar10Image(img, path):
+    img = img.asnumpy().transpose(1, 2, 0)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    cv2.imwrite(path, img)
+
+
+def openCifar10():
+    categories = getCifar10Cat()
+    filePaths = filedialog.askopenfilenames(title="Select the corresponding dataset(s)", initialdir="/media/deblazz/CEBAE294BAE2787")
+    imagesCount = 0
+    for filePath in filePaths:
+        with open(filePath, 'rb') as file:
+            dsContent = pickle.load(file, encoding='bytes')
+        imgs = dsContent[b'data']
+        imgs = numpy.reshape(imgs, (10000, 3, 32, 32))
+        lbls = dsContent[b'labels']
+        imgArr = mxnet.nd.array(imgs)
+        lblArr = mxnet.nd.array(lbls)
+        for img in imgArr:
+            path = f"./training/cifar10/img{imagesCount}.jpg"
+            saveCifar10Image(img, path)
+            imagesCount+=1
+
+
+
+
+
+
 
 def saveNegatives():
     messagebox.showinfo("Info", "Select your negative samples, you'll need at least 500 of them to get a good result")
